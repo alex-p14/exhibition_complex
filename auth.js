5c52e42582f04f5b35776333ea1cd5bb7c71a270
Tirion <tirion2546@gmail.com>
2023-05-20 02:18:46 +0300

    Bumped version number to 1.0.1

--- auth.js
+++ auth.js
@@ -3,6 +3,6 @@ n autenticate (login, password) {
 	if (login === 'login' && password === 'password') {
 		return 'You were logged in';
 	} else {
-		return 'login id incorrect';
+		return 'login id incorrect!';
 	}
 }
